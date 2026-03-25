18e9bec313fce9943b3f2a843e86008ba861cdc9
Kozinov Sergey <sesekozinov@edu.hse.ru>
2026-03-26 02:59:17 +0300

    fix: editor width

--- frontend/src/components/CodeEditor.jsx
+++ frontend/src/components/CodeEditor.jsx
@@ -181,7 +181,7 @@ const CodeEditor = ({ user }) => {
                     }}
                 />
 
-                <Box flex={1}>
+                <Box flex={2}>
                     <Editor
                         height="100vh"
                         theme={colorMode === "dark" ? "vs-dark" : "light"}
@@ -197,7 +197,7 @@ const CodeEditor = ({ user }) => {
                     />
                 </Box>
 
-                <VStack w="50%" spacing={2} alignItems="stretch" height="75vh">
+                <VStack w="40%" spacing={2} alignItems="stretch" height="75vh">
                     <Input stdin={stdin} onStdin={setStdin} />
                     <Output output={output} loading={loading} />
                 </VStack>
