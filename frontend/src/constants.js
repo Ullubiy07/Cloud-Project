caa9040797c0e3e8b4b00412e071413306130385
Kozinov Sergey <sesekozinov@edu.hse.ru>
2026-03-26 00:27:01 +0300

    fix: assmebly

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,7 +2,7 @@ export const LANGUAGES = {
     python: "3.12",
     javascript: "18.19.0",
     c: "",
-    asm: "NASM",
+    assembly: "NASM",
     sql: "",
     cpp: "C++20",
 };
@@ -12,7 +12,7 @@ export const SNIPPETS = {
     python: `# Here you can write your code\nprint("Hello, Cloud Comptuting!")\n`,
     cpp: `// Here you can write your code\n#include <iostream>\n\nint main() {\n\tstd::cout << "Hello, Cloud Computing!";\n\treturn 0;\n}`,
     c: `// Here you can write your code\n#include <stdio.h>\n\nint main(void) {\n\tprintf("Hello, Cloud Computing!");\n\treturn 0;\n}`,
-    asm: `; Here you can write your code\n\nsection .text\nglobal main\nmain:\n\txor eax, eax\n\tret\n`,
+    assembly: `; Here you can write your code\nsection .data\n\tmsg db 'Hello, World!', 0\n\nsection .text\n\textern io_print_string\n\textern io_newline\n\tglobal main\n\nmain:\n\tpush rbp\n\tmov rbp, rsp\n\t\n\n\tmov rdi, msg\n\tcall io_print_string\n\tcall io_newline\n\n\tmov rsp, rbp\n\tpop rbp\n\txor eax, eax\n\tret\n`,
     sql: ``,
 }
 
@@ -21,6 +21,6 @@ export const DEFAULT_FILES = {
     javascript: "main.js",
     cpp: "main.cpp",
     c: "main.c",
-    asm: "main.asm",
+    assembly: "main.asm",
     sql: "main.sql",
 };
